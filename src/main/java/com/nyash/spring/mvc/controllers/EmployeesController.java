09b283431164daa63eddae00084e3b3a11704a9c
Nyaashmyaash <81423982+Nyaashmyaash@users.noreply.github.com>
2021-06-06 07:39:35 +0700

    add changes

--- src/main/java/com/nyash/spring/mvc/controllers/EmployeesController.java
+++ src/main/java/com/nyash/spring/mvc/controllers/EmployeesController.java
@@ -5,21 +5,35 @@ import com.nyash.spring.mvc.services.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
-@RequestMapping("/employees")
 public class EmployeesController {
 
     @Autowired
     private EmployeeService employeeService;
 
-    @RequestMapping("/")
+
+    @RequestMapping ("/")
     public String showAllEmployees(Model model) {
         List<Employee> allEmployees = employeeService.getAllEmployees();
-        model.addAttribute("allEmps", allEmployees);
-        return "employees/all-employees";
+        model.addAttribute("allEmployees", allEmployees);
+        return "all-employees";
+    }
+
+    @RequestMapping("/addNewEmployee")
+    public String addNewEmployee(Model model) {
+        Employee employee = new Employee();
+        model.addAttribute("employee", employee);
+        return "employee-info";
+    }
+
+    @RequestMapping ("/saveEmployee")
+    public String saveEmployee(@ModelAttribute("employee") Employee employee) {
+        employeeService.saveEmployee(employee);
+        return "redirect:/";
     }
 }
